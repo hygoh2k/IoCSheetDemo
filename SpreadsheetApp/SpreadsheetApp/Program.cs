4be6b523839db420f8439a2f92f931a1b0655bb7
Goh <uids8115@contiwan.com>
2019-10-02 23:38:07 +0800

    1) RegisterComponents:
       register SpreadsheetCommand, SpreadsheetCommand, SpreadsheetCommand, SpreadsheetCollection into dependency
    2) add command  line interaction

--- SpreadsheetApp/SpreadsheetApp/Program.cs
+++ SpreadsheetApp/SpreadsheetApp/Program.cs
@@ -1,16 +1,48 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.Resolvers.SpecializedResolvers;
 using Castle.Windsor;
+using Common.Model;
+using Spreadsheet.Model;
+using Spreadsheet.Model.Spreadsheet.Model;
 
 namespace SpreadsheetApp
 {
     class Program
     {
         private static Castle.Windsor.WindsorContainer _container;
+
+        static void RegisterComponents()
+        {
+            _container.Register(Component.For<SpreadsheetCommand>()
+                .ImplementedBy<CreateSpreadsheetCommand>()
+                .DependsOn(Dependency.OnValue("cmdKey", "C"))
+            );
+
+            _container.Register(Component.For<SpreadsheetCommand>()
+                .ImplementedBy<UpdateSpreadsheetCommand>()
+                .DependsOn(Dependency.OnValue("cmdKey", "N"))
+
+            );
+
+            _container.Register(Component.For<SpreadsheetCommand>()
+                .ImplementedBy<SumSpreadsheetCommand>()
+                .DependsOn(Dependency.OnValue("cmdKey", "S"))
+
+            );
+
+            _container.Register(Component.For<SpreadsheetCollection>());
+
+            _container.Register(Component.For<ISheetPrinter>()
+                .ImplementedBy<SpreadsheetPrinter>());
+
+
+        }
 
         static void SetupContainer()
         {
@@ -23,12 +55,51 @@ namespace SpreadsheetApp
         {
             //setup Dependency Container
             SetupContainer();
-
+            //register Dependencies
+            RegisterComponents();
 
             int exitCode = 0x0;
             try
             {
-                
+                var sheetManager = _container.Resolve<SpreadsheetCollection>();
+                var sheetPrinter = _container.Resolve<ISheetPrinter>();
+
+                Console.WriteLine("Enter command");
+                ISheet currentSheet = null;
+                while (true)
+                {
+                    Console.Write(">");
+                    string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    string cmdKey = input.Length > 0 ? input[0] : "";
+
+                    if (cmdKey.Equals("Q"))
+                    {
+                        break;
+                    }
+
+                    string[] cmdArgs = input.Length > 1 ? input.Skip(1).ToArray() : new string[] { };
+
+                    if (sheetManager.CommandCollection.ContainsKey(cmdKey))
+                    {
+                        var cmd = sheetManager.CommandCollection[cmdKey];
+                        var result = cmd.Execute(currentSheet, cmdArgs);
+                        if (result.Success)
+                        {
+                            currentSheet = result.Spreadsheet;
+                            sheetPrinter.PrintContent(currentSheet, Console.Out);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(result.ErrorMessage);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Command");
+                    }
+
+                }
             }
             catch (Exception ex)
             {
