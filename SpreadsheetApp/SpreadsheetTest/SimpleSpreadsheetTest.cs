fe549a11d3a6034e103b72810bbecacf5fa6aa87
Goh <uids8115@contiwan.com>
2019-10-02 23:58:38 +0800

    add unit test

---
+++ SpreadsheetApp/SpreadsheetTest/SimpleSpreadsheetTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spreadsheet.Model;
+
+namespace SpreadsheetTest
+{
+    [TestClass]
+    public class SimpleSpreadsheetTest
+    {
+        [TestMethod]
+        public void TestNormalUpdate()
+        {
+            var sheet = new SimpleSpreadsheet(2, 3);
+            sheet[0, 0] = 1;
+            sheet[0, 1] = 2;
+            Assert.AreEqual(sheet[0, 0], 1);
+            Assert.AreEqual(sheet[0, 1], 2);
+            Assert.AreEqual(sheet[0, 2], 0);
+
+            Assert.AreEqual(sheet[1, 0], 0);
+            Assert.AreEqual(sheet[1, 1], 0);
+            Assert.AreEqual(sheet[1, 2], 0);
+
+        }
+
+        [TestMethod]
+        public void TestIndexOutOfRangeException()
+        {
+            var sheet = new SimpleSpreadsheet(2, 3);
+            sheet[0, 0] = 1;
+            sheet[0, 1] = 2;
+
+            try
+            {
+                var result = sheet[2, 0];
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is IndexOutOfRangeException);
+            }
+
+            try
+            {
+                var result = sheet[0, 3];
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is IndexOutOfRangeException);
+            }
+
+        }
+    }
+}
